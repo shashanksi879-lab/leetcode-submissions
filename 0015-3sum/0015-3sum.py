37503c8050cadef0572ee99fe44c2ebbe0cdb0fe
shashanksi879-lab <shashanksi879@gmail.com>
2026-09-19 22:27:17 +0530

    Time: 623 ms (62.69%), Space: 22.4 MB (33%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         n = len(nums)
-        result = set()  # stores unique sorted triplets
+        nums.sort()  # sort first for two-pointer technique
 
         for i in range(n):
-            hashset = set()   # fresh set for each fixed i
-            for j in range(i + 1, n):
-                third = -(nums[i] + nums[j])  # value we need
-                if third in hashset:
-                    temp = [nums[i], nums[j], third]
-                    temp.sort()
-                    result.add(tuple(temp))
-                # add current nums[j] for future pairs
-                hashset.add(nums[j])
+            # skip duplicate fixed elements
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
 
-        ans = list(result)
+            # set up the two pointers
+            j = i + 1
+            k = n - 1
+
+            # move pointers toward each other
+            while j < k:
+                total_sum = nums[i] + nums[j] + nums[k]
+
+                if total_sum < 0:
+                    j += 1                     # need a larger sum
+                elif total_sum > 0:
+                    k -= 1                     # need a smaller sum
+                else:
+                    # found a valid triplet
+                    temp = [nums[i], nums[j], nums[k]]
+                    ans.append(temp)
+
+                    # move both pointers and skip duplicates
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
         return ans
